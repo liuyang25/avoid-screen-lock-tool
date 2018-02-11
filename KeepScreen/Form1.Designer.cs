c58e7dd975c68ba2837ea97c4bb5f72509462485
liuyang <liuyang@hdfax.com>
2018-02-11 11:49:58 +0800

    first version

---
+++ KeepScreen/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace KeepScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.激活ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.notifyMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.notifyMenu;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "防锁屏";
+            this.notifyIcon.Visible = true;
+            // 
+            // notifyMenu
+            // 
+            this.notifyMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.激活ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.notifyMenu.Name = "notifyMenu";
+            this.notifyMenu.Size = new System.Drawing.Size(125, 48);
+            // 
+            // 激活ToolStripMenuItem
+            // 
+            this.激活ToolStripMenuItem.Name = "激活ToolStripMenuItem";
+            this.激活ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.激活ToolStripMenuItem.Text = "取消激活";
+            this.激活ToolStripMenuItem.Click += new System.EventHandler(this.activeSwich_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.toolMenuCancel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Enabled = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.notifyMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip notifyMenu;
+        private System.Windows.Forms.ToolStripMenuItem 激活ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+    }
+}
+
---
+++ KeepScreen/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Timers;
+
+namespace KeepScreen
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32")]
+        private static extern int mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("User32.dll", EntryPoint = "FindWindow")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll", EntryPoint = "FindWindowEx")]   //找子窗体   
+        private static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [DllImport("User32.dll", EntryPoint = "SendMessage")]   //用于发送信息给窗体   
+        private static extern int SendMessage(IntPtr hWnd, int Msg, IntPtr wParam, string lParam);
+
+        [DllImport("User32.dll", EntryPoint = "ShowWindow")]   //
+        private static extern bool ShowWindow(IntPtr hWnd, int type);
+
+        private const int MOUSEEVENTF_MOVE = 0x0001;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            Run();
+        }
+        public bool activeState = true;
+        private void Run()
+        {
+            int TimerInterval = 30;
+
+            System.Timers.Timer timer = new System.Timers.Timer(); // 定义定时器
+            timer.Enabled = true; // 启用定时器
+            timer.Interval = 1000 * TimerInterval; // 设定时间间隔（毫秒：1秒 = 1000毫秒）
+            //timer.Interval = TimerInterval; // Test
+
+            timer.Elapsed += new ElapsedEventHandler(moveMouseHandler); // 等到间隔时间到，执行
+
+        }
+        private void moveMouseHandler(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            if (this.activeState)
+            {
+                // 将鼠标移动一个像素
+                mouse_event(MOUSEEVENTF_MOVE, 1, 1, 0, 0);
+                mouse_event(MOUSEEVENTF_MOVE, -1, -1, 0, 0);
+            }
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            switch (e.Button) {
+                case MouseButtons.Left:
+                case MouseButtons.Right:
+                    notifyMenu.Show();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void toolMenuCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void activeSwich_Click(object sender, EventArgs e)
+        {
+            this.activeState = !this.activeState;
+            ((ToolStripMenuItem)sender).Text = this.activeState ? "取消激活" : "激活";
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
