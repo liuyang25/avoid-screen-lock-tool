49b1d89967b9eb9a86a3e0341bcc2362124c5dd0
liuyang <liuyang@hdfax.com>
2018-02-26 15:33:55 +0800

    add custom lock screen timer.

--- KeepScreen/Form1.Designer.cs
+++ KeepScreen/Form1.Designer.cs
@@ -32,9 +32,15 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
             this.notifyMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.设置锁屏时间ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.激活ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.confirm_button = new System.Windows.Forms.Button();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.notifyMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // notifyIcon
@@ -47,36 +53,108 @@
             // notifyMenu
             // 
             this.notifyMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.设置锁屏时间ToolStripMenuItem,
             this.激活ToolStripMenuItem,
             this.退出ToolStripMenuItem});
             this.notifyMenu.Name = "notifyMenu";
-            this.notifyMenu.Size = new System.Drawing.Size(125, 48);
+            this.notifyMenu.Size = new System.Drawing.Size(183, 70);
+            // 
+            // 设置锁屏时间ToolStripMenuItem
+            // 
+            this.设置锁屏时间ToolStripMenuItem.Name = "设置锁屏时间ToolStripMenuItem";
+            this.设置锁屏时间ToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
+            this.设置锁屏时间ToolStripMenuItem.Text = "设置锁屏时间(30分)";
+            this.设置锁屏时间ToolStripMenuItem.Click += new System.EventHandler(this.ToolMenuSetLocktime_Click);
             // 
             // 激活ToolStripMenuItem
             // 
             this.激活ToolStripMenuItem.Name = "激活ToolStripMenuItem";
-            this.激活ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.激活ToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
             this.激活ToolStripMenuItem.Text = "取消激活";
-            this.激活ToolStripMenuItem.Click += new System.EventHandler(this.activeSwich_Click);
+            this.激活ToolStripMenuItem.Click += new System.EventHandler(this.ActiveSwich_Click);
             // 
             // 退出ToolStripMenuItem
             // 
             this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
-            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(182, 22);
             this.退出ToolStripMenuItem.Text = "退出";
-            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.toolMenuCancel_Click);
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.ToolMenuCancel_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(82, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 21);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // confirm_button
+            // 
+            this.confirm_button.Location = new System.Drawing.Point(47, 39);
+            this.confirm_button.Name = "confirm_button";
+            this.confirm_button.Size = new System.Drawing.Size(75, 23);
+            this.confirm_button.TabIndex = 2;
+            this.confirm_button.Text = "确定";
+            this.confirm_button.UseVisualStyleBackColor = true;
+            this.confirm_button.Click += new System.EventHandler(this.confirm_button_Click);
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancel_button.Location = new System.Drawing.Point(157, 39);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(75, 23);
+            this.cancel_button.TabIndex = 3;
+            this.cancel_button.Text = "取消";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.cancel_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "分";
             // 
             // Form1
             // 
+            this.AcceptButton = this.confirm_button;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Enabled = false;
+            this.CancelButton = this.cancel_button;
+            this.ClientSize = new System.Drawing.Size(284, 72);
+            this.ControlBox = false;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.confirm_button);
+            this.Controls.Add(this.numericUpDown1);
+            this.KeyPreview = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "设置锁屏时间";
             this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.notifyMenu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -86,6 +164,11 @@
         private System.Windows.Forms.ContextMenuStrip notifyMenu;
         private System.Windows.Forms.ToolStripMenuItem 激活ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 设置锁屏时间ToolStripMenuItem;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button confirm_button;
+        private System.Windows.Forms.Button cancel_button;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- KeepScreen/Form1.cs
+++ KeepScreen/Form1.cs
@@ -29,28 +29,43 @@ namespace KeepScreen
         [DllImport("User32.dll", EntryPoint = "ShowWindow")]   //
         private static extern bool ShowWindow(IntPtr hWnd, int type);
 
+        [DllImport("User32.dll", EntryPoint = "LockWorkStation")]   //
+        private static extern bool LockWorkStation();
+
         private const int MOUSEEVENTF_MOVE = 0x0001;
 
+        // 上一位置
+        private int last_mouse_x = 0;
+        private int last_mouse_y = 0;
+        private long last_mouse_timepoint = 0;
+
+        // 锁屏定时器 (分)
+        private int screen_lock_time = 30;
 
         public Form1()
         {
             InitializeComponent();
+            this.screen_lock_time = Properties.Settings.Default.screen_lock_time;
+            this.设置锁屏时间ToolStripMenuItem.Text = "设置锁屏时间(" + this.screen_lock_time.ToString() + ")";
+
             Run();
         }
         public bool activeState = true;
         private void Run()
         {
-            int TimerInterval = 30;
+            int time_interval1 = 30;
 
-            System.Timers.Timer timer = new System.Timers.Timer(); // 定义定时器
-            timer.Enabled = true; // 启用定时器
-            timer.Interval = 1000 * TimerInterval; // 设定时间间隔（毫秒：1秒 = 1000毫秒）
-            //timer.Interval = TimerInterval; // Test
+            System.Timers.Timer timer1 = new System.Timers.Timer(); // 定义定时器
+            timer1.Enabled = true; // 启用定时器
+            timer1.Interval = 1000 * time_interval1; // 设定时间间隔（毫秒：1秒 = 1000毫秒）
+            timer1.Elapsed += new ElapsedEventHandler(MoveMouseHandler); // 等到间隔时间到，执行
 
-            timer.Elapsed += new ElapsedEventHandler(moveMouseHandler); // 等到间隔时间到，执行
-
+            System.Timers.Timer timer2 = new System.Timers.Timer();
+            timer2.Enabled = true;
+            timer2.Interval = 1000;
+            timer2.Elapsed += new ElapsedEventHandler(CheckActive);
         }
-        private void moveMouseHandler(object sender, System.Timers.ElapsedEventArgs e)
+        private void MoveMouseHandler(object sender, System.Timers.ElapsedEventArgs e)
         {
             if (this.activeState)
             {
@@ -59,8 +74,27 @@ namespace KeepScreen
                 mouse_event(MOUSEEVENTF_MOVE, -1, -1, 0, 0);
             }
         }
+        private void CheckActive(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            last_mouse_timepoint++;
 
-        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+            int x = Control.MousePosition.X;
+            int y = Control.MousePosition.Y;
+            if (x != last_mouse_x || y != last_mouse_y)
+            {
+                last_mouse_x = x;
+                last_mouse_y = y;
+                last_mouse_timepoint = 0;
+            }
+            else if (last_mouse_timepoint > screen_lock_time * 60) 
+            {
+                last_mouse_timepoint = 0;
+                LockWorkStation(); // 锁屏
+                //MessageBox.Show("Test:"+ DateTime.Now.ToFileTimeUtc().ToString() + "\n" + last_mouse_timepoint);
+            }
+        }
+
+        private void NotifyIcon_MouseClick(object sender, MouseEventArgs e)
         {
             switch (e.Button) {
                 case MouseButtons.Left:
@@ -72,12 +106,12 @@ namespace KeepScreen
             }
         }
 
-        private void toolMenuCancel_Click(object sender, EventArgs e)
+        private void ToolMenuCancel_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            Environment.Exit(0);
         }
 
-        private void activeSwich_Click(object sender, EventArgs e)
+        private void ActiveSwich_Click(object sender, EventArgs e)
         {
             this.activeState = !this.activeState;
             ((ToolStripMenuItem)sender).Text = this.activeState ? "取消激活" : "激活";
@@ -86,6 +120,32 @@ namespace KeepScreen
         private void Form1_Shown(object sender, EventArgs e)
         {
             this.Hide();
+        }
+
+        private void ToolMenuSetLocktime_Click (object sender, EventArgs e)
+        {
+            this.numericUpDown1.Value = Properties.Settings.Default.screen_lock_time;
+            this.Show();
+        }
+
+        private void confirm_button_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.screen_lock_time = (int)numericUpDown1.Value;
+            Properties.Settings.Default.Save();
+            this.screen_lock_time = Properties.Settings.Default.screen_lock_time;
+            this.设置锁屏时间ToolStripMenuItem.Text = "设置锁屏时间(" + this.screen_lock_time.ToString() + ")";
+
+            this.Hide();
+        }
+
+        private void cancel_button_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            last_mouse_timepoint = 0;
         }
     }
 }
